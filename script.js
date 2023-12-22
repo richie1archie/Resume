799e7690b5b3d1af5c889e87952dbf7d9a25e5bc
richie1archie <49341228+richie1archie@users.noreply.github.com>
2023-12-22 16:31:06 +0100

    add

--- script.js
+++ script.js
@@ -3,19 +3,3 @@ function toggleDetails(id) {
     details.style.display = (details.style.display === 'none') ? 'block' : 'none';
 }
 
-function downloadResume() {
-    var element = document.getElementById('resume'); // Replace 'resume' with the ID of your resume container
-    
-    // Use html2pdf's promise-based API for better control
-    html2pdf(element)
-        .from(element)
-        .outputPdf()
-        .then(function(pdf) {
-            // Trigger the download
-            var blob = new Blob([pdf], { type: 'application/pdf' });
-            var link = document.createElement('a');
-            link.href = window.URL.createObjectURL(blob);
-            link.download = 'resume.pdf';
-            link.click();
-        });
-}
